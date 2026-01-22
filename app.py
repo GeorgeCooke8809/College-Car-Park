7573cdfe9de73888e21eea32fb495734cc745e16
George_Cooke <georgecooke8809@gmail.com>
2026-01-22 15:54:06 +0000

    Made view all bookings page

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import flask
-from flask import Flask, request
+from flask import Flask, request, redirect
 from backend import connection
 import datetime
 
@@ -12,27 +12,10 @@ data = connection()
 
 @app.route("/", methods = ["GET"])
 def index():
-    date = flask.request.args.get("date", default="None", type=str)
-
-    if date == "None":
-        today = datetime.date.today()
-        date = today.strftime("%d/%m/%Y")
-    else:
-        date=f"{date[0:2]}/{date[2:4]}/{date[-4:]}"
-
-    print(f"{date = }")
-
-    return flask.render_template("admin-dashboard.html",
-                                 current_date = date,
-                                 current_spaces = 1,
-                                 maximum_spaces = 200,
-                                 bookings = data.get_all_date_bookings(date)
-                                 )
+    return redirect("./admin-dashboard", code=302)
 
 @app.route("/admin-dashboard", methods = ["GET", "POST"])
 def admin_dashboard():
-    #TODO: copy from index
-
     date = flask.request.args.get("date", default="None", type=str)
 
     if date == "None":
@@ -58,9 +41,26 @@ def view_user():
         raise Exception("ERROR: No User ID provided")
     else:
         return flask.render_template("admin-view-user.html",
-                                 user_data=data.get_user_profile_data(userID),
-                                 user_cars=data.get_all_user_cars(userID) 
-                                 )
+                                     userId=userID,
+                                     user_data=data.get_user_profile_data(userID),
+                                     user_cars=data.get_all_user_cars(userID) 
+                                     )
+    
+@app.route("/admin-view-user-bookings", methods = ["GET", "POST"])
+def view_user_bookings():
+    userID = flask.request.args.get("uid", default="None", type=str)
+
+    if userID == "None":
+        raise Exception("ERROR: No User ID provided")
+    else:
+        profile_data = data.get_user_profile_data(userID)
+
+        name = f"{profile_data["First Name"]} {profile_data["Last Name"]}"
+
+        return flask.render_template("admin-view-user-bookings.html",
+                                name = name,
+                                bookings=data.get_all_user_bookings(userID)
+                                )
 
 if __name__ == "__main__":
     app.run(debug=True)
